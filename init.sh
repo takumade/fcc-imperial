0e160189db866be1acd8d2a8fe5d0d0b16c43269
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-05-15 05:57:40 +0200

    first commit

---
+++ init.sh
@@ -0,0 +1,7 @@
+echo "# fcc-imperial" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/takumade/fcc-imperial.git
+git push -u origin main
